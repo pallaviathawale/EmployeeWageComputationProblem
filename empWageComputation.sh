42bce842542daa2131ead835a00db9256a788f28
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-02-09 23:15:07 +0530

    Add: DailyWageAlongWithTotalWage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,18 +1,29 @@
 echo "Welcome to Employee Wage Computation Program"
-﻿
-function getWorkHours()
+﻿function getWorkHours()
 {
         local startTime=$1
         local endTime=$2
+        local ratePerHour=$3
 
         local startHour=$(echo $startTime | awk -F: '{print $1}')
         local endHour=$(echo $endTime | awk -F: '{print $1}')
 
         local workHours=$((endHour-startHour))
-        echo "Work hours: $workHours"
+        local dailyWage=$((workHours*ratePerHour))
+        echo "$dailyWage"
 }
 
 startTime="09:00"
 endTime="17:00"
+ratePerHour=20
 
-getWorkHours "$startTime" "$endTime"
+totalWage=0
+workingDays=20
+
+for ((day=1; day<=workingDays; day++))
+do
+        dailyWage=$(getWorkHours "$startTime" "$endTime" $ratePerHour)
+        totalWage=$((totalWage+dailyWage))
+done
+
+echo "Total wage: $totalWage"
