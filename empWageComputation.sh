f248484a50fe7a9cf9e897486e1305ed3d88075c
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-02-09 16:53:48 +0530

    Add: Add Part time Employee & Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,18 +1,36 @@
 echo "Welcome to Employee Wage Computation Program"
-isPresent=$((RANDOM%3));
 perHourSalary=20;
 workingHour=0;
-if [ $isPresent -eq 0 ]
-then
-	echo "Employee is absent";
-	workingHour=0;
-elif [ $isPresent -eq 1 ]
-then
-	echo "Employee is present";
-	workingHour=8;
-else
-	echo "Employee is working as part time";
-	workingHour=4;
-fi
-salary=$(($perHourSalary * $workingHour));
-echo "Employee has earned $salary $ today";
+totalSalary=0;
+totalWorkingHour=0;
+day=1;
+while [[ $day -le 20 && $totalWorkingHour -lt 40 ]]
+do
+	isPresent=$((RANDOM%3));
+	case $isPresent in
+		0)
+		#echo "Employee is absent";
+		workingHour=0;
+		;;
+
+		1)
+		#echo "Employee is present";
+		workingHour=8;
+		;;
+
+		2)
+		#echo "Employee is working as part time";
+		workingHour=4;
+		;;
+	esac
+	totalWorkingHour=$(($totalWorkingHour + $workingHour));
+	if [ $totalWorkingHour -gt 40 ]
+	then
+		totalWorkingHour=$(($totalWorkingHour - $workingHour));
+		break;
+	fi
+	salary=$(($perHourSalary * $workingHour));
+	totalSalary=$(($totalSalary + $salary));
+	((day++));
+done
+echo "Employee has earned $totalSalary $ in a month (Total working Hour : $totalWorkingHour)";
