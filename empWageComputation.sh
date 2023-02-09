b9c174b6f3b42a3282cefcc46c422cd4f788d686
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-02-09 19:01:32 +0530

    Add: CalculatingWagesforMonth

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,22 +1,20 @@
 echo "Welcome to Employee Wage Computation Program"
-for files in *
+empRatePerHr=20;
+totalSalary=0;
+numofWorkingDays=20;
+for (( day=1; day<=$numofWorkingDays; day++ ))
 do
-	ext=`echo $files | awk -F. '{print $2}'`;
-	case $ext in
-		txt)
-			echo "$files is .text file";
-		;;
-
-		java)
-			echo "$files is java program file";
-		;;
-
-		sh)
-			echo "$files is shell script file";
-		;;
-
-		*)
-			echo "$files has $ext extension";
-		;;
-	esac
+ attendance=$((RANDOM%3));
+ case $attendance in
+ 1)
+  empHrs=8;;
+ 2)
+  empHrs=4;;
+ *)
+  empHrs=0;
+ esac
+salary=$(($empHrs*$empRatePerHr))
+totalSalary=$(($totalSalary+$salary))
 done
+echo "Employee has earned $totalSalary in a month";
+
