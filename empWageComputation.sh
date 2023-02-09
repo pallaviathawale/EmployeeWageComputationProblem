bd5973408a10d12b7e2cbb2188c64f92f8eea5fd
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-02-09 16:24:47 +0530

    Add: CalculateDailyEmployeeWage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,7 +1,18 @@
 echo "Welcome to Employee Wage Computation Program"
-random=$((RANDOM % 2))
-if [ $random -le 1 ]; then
-  echo "employee is present."
+isPresent=$((RANDOM%3));
+perHourSalary=20;
+workingHour=0;
+if [ $isPresent -eq 0 ]
+then
+	echo "Employee is absent";
+	workingHour=0;
+elif [ $isPresent -eq 1 ]
+then
+	echo "Employee is present";
+	workingHour=8;
 else
-  echo "employee is absent."
+	echo "Employee is working as part time";
+	workingHour=4;
 fi
+salary=$(($perHourSalary * $workingHour));
+echo "Employee has earned $salary $ today";
