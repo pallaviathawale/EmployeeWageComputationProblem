b9affceec45d7d0e17f945c933abce4699350a4f
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-02-09 17:11:01 +0530

    Add: solvingusingSwitchCaseStatement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,36 +1,22 @@
 echo "Welcome to Employee Wage Computation Program"
-perHourSalary=20;
-workingHour=0;
-totalSalary=0;
-totalWorkingHour=0;
-day=1;
-while [[ $day -le 20 && $totalWorkingHour -lt 40 ]]
+for files in *
 do
-	isPresent=$((RANDOM%3));
-	case $isPresent in
-		0)
-		#echo "Employee is absent";
-		workingHour=0;
+	ext=`echo $files | awk -F. '{print $2}'`;
+	case $ext in
+		txt)
+			echo "$files is text file";
 		;;
 
-		1)
-		#echo "Employee is present";
-		workingHour=8;
+		java)
+			echo "$files is java program file";
 		;;
 
-		2)
-		#echo "Employee is working as part time";
-		workingHour=4;
+		sh)
+			echo "$files is shell script file";
+		;;
+
+		*)
+			echo "$files has $ext extension";
 		;;
 	esac
-	totalWorkingHour=$(($totalWorkingHour + $workingHour));
-	if [ $totalWorkingHour -gt 40 ]
-	then
-		totalWorkingHour=$(($totalWorkingHour - $workingHour));
-		break;
-	fi
-	salary=$(($perHourSalary * $workingHour));
-	totalSalary=$(($totalSalary + $salary));
-	((day++));
 done
-echo "Employee has earned $totalSalary $ in a month (Total working Hour : $totalWorkingHour)";
